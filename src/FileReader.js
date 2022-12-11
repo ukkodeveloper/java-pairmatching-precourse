69b8393c7a72a97af45dfd544f180c7d0957189d
ukkodeveloper <ukkodeveloper@gmail.com>
2022-12-11 15:35:41 +0900

    feat: FileReader 추가

---
+++ src/FileReader.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const FileReader = {
+  read(course, callback) {
+    fs.readFile(`./resources/${course}-crew.md`, 'utf8', (err, data) => {
+      if (err) {
+        throw new Error(`[개발자] ${course} 크루 정보 파일이 없습니다.`);
+      }
+      callback(data);
+    });
+  },
+};
+
+module.exports = FileReader;
+
+FileReader('frontend', console.log);
